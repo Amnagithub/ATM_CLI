f7cdb2b35656ae7e268b50c3d3f635d637112e51
Amnagithub <amnaaplus1@gmail.com>
2024-03-30 22:11:33 +0500

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 let balance = 10000;
 let myPin = 1234;
 let anwserPin = await inquirer.prompt([
@@ -6,27 +7,52 @@ let anwserPin = await inquirer.prompt([
         name: "pin",
         message: "Enter your pin",
         type: "number",
-    }
+    },
 ]);
-console.log(anwserPin.pin);
-if (anwserPin === myPin) {
-    console.log("you are Welcome");
+if (anwserPin.pin === myPin) {
+    console.log(chalk.green.bold, (`Welcome`));
     let operationAns = await inquirer.prompt([
         {
             name: "operation",
-            message: "Select One Option",
+            message: "select one option",
             type: "list",
-            choices: ["withdraw", "check balance", "fastcash"]
-        }
+            choices: ["withdraw", "check balance", "fastcash"],
+        },
     ]);
+    console.log(operationAns);
     if (operationAns.operation === "withdraw") {
         let amountAns = await inquirer.prompt([
             {
                 name: "amount",
                 message: "enter your amount",
                 type: "number",
-            }
+            },
         ]);
+        if (amountAns.amount <= balance) {
+            let remainingBal = (balance -= amountAns.amount);
+            console.log(`your remaining balance is :${balance}`);
+        }
+        else {
+            console.log("Insuficient Balance");
+        }
+    }
+    if (operationAns.operation === "check balance") {
+        console.log(`your current balance is :${balance}`);
+    }
+    if (operationAns.operation === "fastcash") {
+        let anscash = await inquirer.prompt([{
+                name: "cash",
+                type: "number",
+                message: "Enter Cash Amount"
+            },
+        ]);
+        if (anscash.cash <= balance) {
+            let remainingcash = balance -= anscash.cash;
+            console.log(`your current balance is :${balance}`);
+        }
+        else {
+            console.log("Insuficient Balance");
+        }
     }
 }
 else {
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 let balance = 10000;
 
@@ -11,18 +12,19 @@ let anwserPin = await inquirer.prompt([
     type: "number",
   },
 ]);
-console.log(anwserPin.pin);
-if (anwserPin === myPin) {
-  console.log("you are Welcome");
+if (anwserPin.pin === myPin) {
+  console.log(chalk.green.bold,(`Welcome`));
 
   let operationAns = await inquirer.prompt([
     {
       name: "operation",
-      message: "Select One Option",
+      message: "select one option",
       type: "list",
       choices: ["withdraw", "check balance", "fastcash"],
     },
   ]);
+  console.log(operationAns);
+
   if (operationAns.operation === "withdraw") {
     let amountAns = await inquirer.prompt([
       {
@@ -31,6 +33,33 @@ if (anwserPin === myPin) {
         type: "number",
       },
     ]);
+    if (amountAns.amount <= balance) {
+      let remainingBal = (balance -= amountAns.amount);
+      console.log(`your remaining balance is :${balance}`);
+    } else {
+      console.log("Insuficient Balance");
+    }
+  }
+
+  if (operationAns.operation === "check balance") {
+    console.log(`your current balance is :${balance}`);
+  }
+
+  if (operationAns.operation === "fastcash") {
+    let anscash = await inquirer.prompt([{
+      name : "cash",
+      type : "number",
+      message : "Enter Cash Amount"
+      },
+
+  ]);
+  if (anscash.cash <= balance) {
+    let remainingcash = balance -= anscash.cash;
+    console.log(`your current balance is :${balance}`);
+  }
+    else {
+      console.log("Insuficient Balance");
+    }
   }
 } else {
   console.log("Invalid Pin");
